c10d1e34c5ecc27107a594cdd2fe0527a1b9a85a
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2020-02-19 15:35:10 +0530

    abs path

--- install.sh
+++ install.sh
@@ -2,20 +2,20 @@
 
 set -e
 
-ROOT_DIR=~/.py-prompts
+ROOT_DIR=$HOME/.py-prompts
 rm -rf $ROOT_DIR
 
 git clone https://github.com/arpitbbhayani/py-prompts $ROOT_DIR
 
-if [ -f ~/.bashrc ]; then
-  echo ".bashrc detected. updating PATH variable and exporting it through ~/.bashrc"
-  echo "export PATH=$PATH:~/.py-prompts" >> ~/.bashrc
+if [ -f $HOME/.bashrc ]; then
+  echo ".bashrc detected. updating PATH variable and exporting it through $HOME/.bashrc"
+  echo "export PATH=$PATH:$HOME/.py-prompts" >> $HOME/.bashrc
   echo "py-prompts is now installed on your machine. call command py-prompts to use it."
-elif [ -f ~/.zshrc ]; then
-  echo ".zshrc detected. updating PATH variable and exporting it through ~/.zshrc"
-  echo "export PATH=$PATH:~/.py-prompts" >> ~/.zshrc
+elif [ -f $HOME/.zshrc ]; then
+  echo ".zshrc detected. updating PATH variable and exporting it through $HOME/.zshrc"
+  echo "export PATH=$PATH:$HOME/.py-prompts" >> $HOME/.zshrc
   echo "py-prompts is now installed on your machine. call command py-prompts to use it."
 else
   echo "could not detect your rc file. kindly add the following line in your rc file"
-  echo "export PATH=$PATH:~/.py-prompts"
+  echo "export PATH=$PATH:$HOME/.py-prompts"
 fi
