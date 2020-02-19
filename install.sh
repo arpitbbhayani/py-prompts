ab0ecd69b22244ff7b1c128ae8bfb77e73f888e0
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2020-02-19 17:28:12 +0530

    pyprompts

--- install.sh
+++ install.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-ROOT_DIR=$HOME/.py-prompts
+ROOT_DIR=$HOME/.pypropmts
 rm -rf $ROOT_DIR
 
-git clone https://github.com/arpitbbhayani/py-prompts $ROOT_DIR
+git clone https://github.com/arpitbbhayani/pypropmts $ROOT_DIR
 
 if [ -f $HOME/.bashrc ]; then
   echo ".bashrc detected. updating PATH variable and exporting it through $HOME/.bashrc"
-  grep ".py-prompts" $HOME/.bashrc > /dev/null
+  grep ".pypropmts" $HOME/.bashrc > /dev/null
   if [ $? -ne 0 ]; then
-    echo "export PATH=$PATH:$HOME/.py-prompts" >> $HOME/.bashrc
+    echo "export PATH=$PATH:$HOME/.pypropmts" >> $HOME/.bashrc
   fi
-  echo "py-prompts is now installed on your machine. call command py-prompts to use it."
+  echo "pypropmts is now installed on your machine. call command pypropmts to use it."
 elif [ -f $HOME/.zshrc ]; then
   echo ".zshrc detected. updating PATH variable and exporting it through $HOME/.zshrc"
-  grep ".py-prompts" $HOME/.bashrc > /dev/null
+  grep ".pypropmts" $HOME/.bashrc > /dev/null
   if [ $? -ne 0 ]; then
-    echo "export PATH=$PATH:$HOME/.py-prompts" >> $HOME/.zshrc
+    echo "export PATH=$PATH:$HOME/.pypropmts" >> $HOME/.zshrc
   fi
-  echo "py-prompts is now installed on your machine. call command py-prompts to use it."
+  echo "pypropmts is now installed on your machine. call command pypropmts to use it."
 else
   echo "could not detect your rc file. kindly add the following line in your rc file"
-  echo "export PATH=$PATH:$HOME/.py-prompts"
+  echo "export PATH=$PATH:$HOME/.pypropmts"
 fi
