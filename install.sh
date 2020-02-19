74585cb8ca7c8309b8e6f0c5bf5cb11e1e3f0a03
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2020-02-19 15:33:04 +0530

    verbose.

--- install.sh
+++ install.sh
@@ -8,9 +8,11 @@ rm -rf $ROOT_DIR
 git clone https://github.com/arpitbbhayani/py-prompts $ROOT_DIR
 
 if [ -f ~/.bashrc ]; then
+  echo ".bashrc detected. updating PATH variable and exporting it through ~/.bashrc"
   echo "export PATH=$PATH:~/.py-prompts" >> ~/.bashrc
   echo "py-prompts is now installed on your machine. call command py-prompts to use it."
 elif [ -f ~/.zshrc ]; then
+  echo ".zshrc detected. updating PATH variable and exporting it through ~/.zshrc"
   echo "export PATH=$PATH:~/.py-prompts" >> ~/.zshrc
   echo "py-prompts is now installed on your machine. call command py-prompts to use it."
 else
