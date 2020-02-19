d4b2099753a0d4acb65d8dfd0d80c5de236075e8
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2020-02-19 16:36:01 +0530

    adding to path if does not already added.

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -e
-
 ROOT_DIR=$HOME/.py-prompts
 rm -rf $ROOT_DIR
 
@@ -9,11 +7,17 @@ git clone https://github.com/arpitbbhayani/py-prompts $ROOT_DIR
 
 if [ -f $HOME/.bashrc ]; then
   echo ".bashrc detected. updating PATH variable and exporting it through $HOME/.bashrc"
-  echo "export PATH=$PATH:$HOME/.py-prompts" >> $HOME/.bashrc
+  grep ".py-prompts" $HOME/.bashrc
+  if [ $? -ne 0 ]; then
+    echo "export PATH=$PATH:$HOME/.py-prompts" >> $HOME/.bashrc
+  fi
   echo "py-prompts is now installed on your machine. call command py-prompts to use it."
 elif [ -f $HOME/.zshrc ]; then
   echo ".zshrc detected. updating PATH variable and exporting it through $HOME/.zshrc"
-  echo "export PATH=$PATH:$HOME/.py-prompts" >> $HOME/.zshrc
+  grep ".py-prompts" $HOME/.bashrc
+  if [ $? -ne 0 ]; then
+    echo "export PATH=$PATH:$HOME/.py-prompts" >> $HOME/.zshrc
+  fi
   echo "py-prompts is now installed on your machine. call command py-prompts to use it."
 else
   echo "could not detect your rc file. kindly add the following line in your rc file"
