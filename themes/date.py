a915bbaa58c8f239c7884d95eb4e0a5ad64414bf
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2020-02-19 20:39:36 +0530

    date theme

---
+++ themes/date.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import sys
+from datetime import datetime
+
+class DatePromptPS1(object):
+  def __str__(self):
+    return "\033[92m[%s]:\033[0m " % (datetime.now().strftime('%H:%M'))
+
+sys.ps1 = DatePromptPS1()
+sys.ps2 = "    \033[91m...\033[0m "
