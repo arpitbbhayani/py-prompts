6ad17970d2af1e81f1df17f3fc8a2ba483498fcd
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2020-02-19 19:06:19 +0530

    removing -e

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ rm -rf $ROOT_DIR
 
 rc_add() {
   name=$1
-  echo -e ".bashrc detected. updating \033[1;33mPATH\033[0m variable and exporting it through $HOME/$name"
-  echo -e "export PATH=$PATH:$HOME/.py-prompts" >> $HOME/$name
-  echo -e "\033[1;33mpy-prompts\033[0m is now installed on your machine."
+  echo ".bashrc detected. updating \033[1;33mPATH\033[0m variable and exporting it through $HOME/$name"
+  echo "export PATH=$PATH:$HOME/.py-prompts" >> $HOME/$name
+  echo "\033[1;33mpy-prompts\033[0m is now installed on your machine."
 }
 
-echo -e "Cloning repository: \033[1;33mhttps://github.com/arpitbbhayani/py-prompts\033[0m at $HOME/.bashrc"
+echo "Cloning repository: \033[1;33mhttps://github.com/arpitbbhayani/py-prompts\033[0m at $HOME/.bashrc"
 git clone https://github.com/arpitbbhayani/py-prompts $ROOT_DIR 2> /dev/null
 
 if [ -f $HOME/.bashrc ]; then
@@ -18,18 +18,18 @@ if [ -f $HOME/.bashrc ]; then
   if [ $? -ne 0 ]; then
     rc_add .bashrc
   else
-    echo -e "\033[0;34mpy-prompts is already installed.\033[0m"
+    echo "\033[0;34mpy-prompts is already installed.\033[0m"
   fi
 elif [ -f $HOME/.zshrc ]; then
   grep -q ".py-prompts" $HOME/.zshrc
   if [ $? -ne 0 ]; then
     rc_add .zshrc
   else
-    echo -e "\033[0;34mpy-prompts is already installed.\033[0m"
+    echo "\033[0;34mpy-prompts is already installed.\033[0m"
   fi
 else
-  echo -e ""
-  echo -e "\033[0;31mCould not detect your rc file. kindly add the following line in your rc file.\033[0m"
-  echo -e ""
-  echo -e "\033[0;34mexport PATH=$PATH:$HOME/.py-prompts\033[0m"
+  echo ""
+  echo "\033[0;31mCould not detect your rc file. kindly add the following line in your rc file.\033[0m"
+  echo ""
+  echo "\033[0;34mexport PATH=$PATH:$HOME/.py-prompts\033[0m"
 fi
