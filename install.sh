1a84b3399cb6da3f9367c2af273373e6a3552faa
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2020-02-19 18:24:44 +0530

    coloring install.

--- install.sh
+++ install.sh
@@ -1,27 +1,35 @@
 #!/bin/sh
 
-set -e
-
 ROOT_DIR=$HOME/.py-prompts
 rm -rf $ROOT_DIR
 
-git clone https://github.com/arpitbbhayani/py-prompts $ROOT_DIR
+rc_add() {
+  name=$1
+  echo -e ".bashrc detected. updating \033[1;33mPATH\033[0m variable and exporting it through $HOME/$name"
+  echo -e "export PATH=$PATH:$HOME/.py-prompts" >> $HOME/$name
+  echo -e "\033[1;33mpy-prompts\033[0m is now installed on your machine."
+}
+
+echo -e "Cloning repository: \033[1;33mhttps://github.com/arpitbbhayani/py-prompts\033[0m at $HOME/.bashrc"
+git clone https://github.com/arpitbbhayani/py-prompts $ROOT_DIR 2> /dev/null
 
 if [ -f $HOME/.bashrc ]; then
-  echo ".bashrc detected. updating PATH variable and exporting it through $HOME/.bashrc"
-  grep ".py-prompts" $HOME/.bashrc > /dev/null
+  grep -q ".py-prompts" $HOME/.bashrc
   if [ $? -ne 0 ]; then
-    echo "export PATH=$PATH:$HOME/.py-prompts" >> $HOME/.bashrc
+    rc_add .bashrc
+  else
+    echo -e "\033[0;34mpy-prompts is already installed.\033[0m"
   fi
-  echo "py-prompts is now installed on your machine. call command py-prompts to use it."
 elif [ -f $HOME/.zshrc ]; then
-  echo ".zshrc detected. updating PATH variable and exporting it through $HOME/.zshrc"
-  grep ".py-prompts" $HOME/.bashrc > /dev/null
+  grep -q ".py-prompts" $HOME/.zshrc
   if [ $? -ne 0 ]; then
-    echo "export PATH=$PATH:$HOME/.py-prompts" >> $HOME/.zshrc
+    rc_add .zshrc
+  else
+    echo -e "\033[0;34mpy-prompts is already installed.\033[0m"
   fi
-  echo "py-prompts is now installed on your machine. call command py-prompts to use it."
 else
-  echo "could not detect your rc file. kindly add the following line in your rc file"
-  echo "export PATH=$PATH:$HOME/.py-prompts"
+  echo -e ""
+  echo -e "\033[0;31mCould not detect your rc file. kindly add the following line in your rc file.\033[0m"
+  echo -e ""
+  echo -e "\033[0;34mexport PATH=$PATH:$HOME/.py-prompts\033[0m"
 fi
